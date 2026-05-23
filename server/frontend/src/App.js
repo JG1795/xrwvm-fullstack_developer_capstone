12adef9d39264bd81aa49dc5c8604c53152001ac
JG1795 <shido.galarza@gmail.com>
2026-05-23 02:11:50 -0400

    Update App.js

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,8 +1,8 @@
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
 import Register from "./components/Register/Register";
-import Dealers from './components/Dealers/Dealers';
-import Dealer from "./components/Dealers/Dealer"
+//import Dealers from './components/Dealers/Dealers';
+//import Dealer from "./components/Dealers/Dealer"
 
 
 
@@ -11,8 +11,8 @@ function App() {
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/dealers" element={<Dealers/>} />
-      <Route path="/dealer/:id" element={<Dealer/>} />
+      //<Route path="/dealers" element={<Dealers/>} />
+      //<Route path="/dealer/:id" element={<Dealer/>} />
 
 
     </Routes>
